5d90c3f911c823218d596f21b27e20b350eed2da
Jessica White <code.jessw@gmail.com>
2017-09-17 18:12:26 +0100

    lists products

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    inventory: [],
+    product: []
+  }
+
+  componentDidMount() {
+    getInventory(this);
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Bravissimo Test</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h5>
+          Product List
+        </h5>
+        <ul>
+          {this.state.inventory.map(item => (
+            <li>{item.name} &pound;{item.price}</li>
+          ))}
+        </ul>
       </div>
     );
   }
+}
+
+function getInventory(component){
+  Axios.get('http://localhost:3001/api/products')
+    .then(({data}) => {
+      component.setState({
+        inventory: data
+      });
+    }).catch((e) => {
+      alert(e);
+    });
 }
 
 export default App;
